8a5050d36b9f551e0c13601f661146a8e5820d7c
VictorManuelSalas <victorsalasnz@gmail.com>
2022-07-19 21:28:23 -0500

    final

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import img2 from '../assets/img2.jpeg'
+import native from '../assets/native.png'
+import react from '../assets/react-galaxia.png'
+
+
 
 const Carrusel = () => {
     return (
         <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <img src="http://dotnettipoftheday.org/wp-content/uploads/2022/02/react-native-735x400.png" className="d-block w-100" alt="..." />
+                    <img src={img2} className="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item">
-                    <img src="https://blog.desafiolatam.com/wp-content/uploads/2019/04/react-galaxia.png" className="d-block w-100" alt="..." />
+                    <img src={native} className="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item">
-                    <img src="https://cdn-images-1.medium.com/max/2000/1*qXcjSfRj0C0ir2yMsYiRyw.jpeg" className="d-block w-100" alt="..." />
+                    <img src={react} className="d-block w-100" alt="..." />
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
